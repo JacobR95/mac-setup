1016bec9a214ba4ff890818f0e2f6d553c0d03b3
JacobR95 <jrobinson@apriori.com>
2023-10-14 16:13:03 -0700

    Rename dotfiles shell script

---
+++ dotfiles/dotfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -sf ~/setup/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/setup/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/setup/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/setup/dotfiles/.zshrc ~/.zshrc
