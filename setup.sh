ac23bd09af47487ea8d32eba03c12665915ae4ed
JacobR95 <jrobinson@apriori.com>
2023-10-19 18:31:19 -0400

    Refactor dotfile linking

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-./dotfiles/dotfiles.sh
+# Symlink dotfiles
+ln -sf ~/setup/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/setup/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/setup/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/setup/dotfiles/.zshrc ~/.zshrc
+
+# Symlink nvim
+ln -sf ~/setup/nvim ~/.config/nvim
