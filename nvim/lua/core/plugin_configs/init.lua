4e45f36ad81ef8498f9400e95e696a3ae5158cb7
JacobR95 <jrobinson@apriori.com>
2023-10-25 19:49:57 -0400

    Improve telescope mapping

--- nvim/lua/core/plugin_configs/init.lua
+++ nvim/lua/core/plugin_configs/init.lua
@@ -1,5 +1,6 @@
 require('core.plugin_configs.colorscheme')
 require('core.plugin_configs.copilot')
+require('core.plugin_configs.commentary')
 require('core.plugin_configs.lsp')
 require('core.plugin_configs.lualine')
 require('core.plugin_configs.nvimtree')
--- nvim/lua/core/plugin_configs/telescope.lua
+++ nvim/lua/core/plugin_configs/telescope.lua
@@ -1,4 +1,48 @@
-require('telescope').setup()
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-j>'] = 'move_selection_next',
+        ['<C-k>'] = 'move_selection_previous',
+      },
+      n = {
+        ['<C-j>'] = 'move_selection_next',
+        ['<C-k>'] = 'move_selection_previous',
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      --@usage don't include the filename in the search results
+      only_sort_text = true,
+    },
+    grep_string = {
+      only_sort_text = true,
+    },
+    planets = {
+      show_pluto = true,
+      show_moon = true,
+    },
+    git_files = {
+      hidden = true,
+      show_untracked = true,
+    },
+    colorscheme = {
+      enable_preview = true,
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                                     -- false will only do exact matching
+      override_generic_sorter = true,                   -- override the generic sorter
+      override_file_sorter = true,                      -- override the file sorter
+      case_mode = "smart_case",                         -- or "ignore_case" or "respect_case"
+    },
+  },
+}
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.git_files, {})
@@ -6,4 +50,3 @@ vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>st', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
